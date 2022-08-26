7a2b82bb8d752fa5b6d6f78de56b947af5304dd8
jefftlin <jefft_lin@163.com>
2022-08-26 20:41:52 +0800

    Fix attempt to connect to data source

--- exchangis-datasource/exchangis-datasource-service/src/main/java/com/webank/wedatasphere/exchangis/datasource/dto/ConnectDataSourceDTO.java
+++ exchangis-datasource/exchangis-datasource-service/src/main/java/com/webank/wedatasphere/exchangis/datasource/dto/ConnectDataSourceDTO.java
@@ -1,11 +1,30 @@
 package com.webank.wedatasphere.exchangis.datasource.dto;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Objects;
+
 public class ConnectDataSourceDTO {
 
     private String userName;
 
     private String datasourceMsg;
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDatasourceMsg() {
+        return datasourceMsg;
+    }
+
+    public void setDatasourceMsg(String datasourceMsg) {
+        this.datasourceMsg = datasourceMsg;
+    }
 
     public ConnectDataSourceDTO() {
     }
@@ -17,6 +36,22 @@ public class ConnectDataSourceDTO {
             sb.append(msg[i]);
         }
         this.datasourceMsg = sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ConnectDataSourceDTO) {
+            ConnectDataSourceDTO conn = (ConnectDataSourceDTO) obj;
+            if (StringUtils.equals(conn.getUserName(), this.userName) && StringUtils.equals(conn.getDatasourceMsg(), this.datasourceMsg)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName);
     }
 
     @Override
